dabcd3f9f5dfc988599cdafa8763e21722e02d48
Dylan <dmccann@ucvts.org>
2020-01-10 11:02:02 -0500

    Checkpoint 1

--- examples/pygame-basics/minimal.py
+++ examples/pygame-basics/minimal.py
@@ -1,17 +1,73 @@
 import sys
-import pygame
+import random
+import pygame as pg
 
-# Initialize pygame so it runs in the background and manages things
-pygame.init()
 
-# Create a display. Size must be a tuple, which is why it's in parentheses
-screen = pygame.display.set_mode( (400, 300) )
+pg.init()
+screen = pg.display.set_mode((640, 480))
 
-# Main loop. Your game would go inside this loop
-while True:
-    # do something for each event in the event queue (list of things that happen)
-    for event in pygame.event.get():
-        # Check to see if the current event is a QUIT event
-        if event.type == pygame.QUIT:
-            # If so, exit the program
-            sys.exit()
+XWING_IMG = pg.Surface((25, 38))
+XWING_IMG.fill((90, 120, 150))
+TIE_IMG = pg.Surface((40, 24))
+TIE_IMG.fill((190, 60, 50))
+BG_COLOR = pg.Color('gray15')
+
+
+def main():
+    clock = pg.time.Clock()
+    # Surfaces/images have a `get_rect` method which 
+    # returns a rect with the dimensions of the image.
+    player_rect = XWING_IMG.get_rect()
+    player_rect.center = (300, 400)
+    change_x = 0
+    change_y = 0
+    enemies = []
+    spawn_counter = 30
+
+    done = False
+
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_d:
+                    change_x = 5
+                if event.key == pg.K_a:
+                    change_x = -5
+            if event.type == pg.KEYUP:
+                if event.key == pg.K_d and change_x > 0:
+                    change_x = 0
+                if event.key == pg.K_a and change_x < 0:
+                    change_x = 0
+
+        # Spawn enemies if counter <= 0 then reset it.
+        spawn_counter -= 1
+        if spawn_counter <= 0:
+            # Append an enemy rect. You can pass the position directly as an argument.
+            enemies.append(TIE_IMG.get_rect(topleft=(random.randrange(600), 0)))
+            spawn_counter = 30
+
+        # Update player_rect and enemies.
+        player_rect.x += change_x
+        player_rect.y += change_y
+        for enemy_rect in enemies:
+            enemy_rect.y += 5
+            # Collision detection with pygame.Rect.colliderect.
+            if player_rect.colliderect(enemy_rect):
+                print('Collision!')
+
+        # Draw everything.
+        screen.fill(BG_COLOR)
+        for enemy_rect in enemies:
+            screen.blit(TIE_IMG, enemy_rect)
+        screen.blit(XWING_IMG, player_rect)
+
+        pg.display.flip()
+        clock.tick(30)
+
+
+if __name__ == '__main__':
+    main()
+    pg.quit()
+    sys.exit()
--- src/game-code.py
+++ src/game-code.py
@@ -1,11 +1,13 @@
 # import pygame module in this program 
 import pygame 
+import time
+import random
   
 # activate the pygame library . 
 # initiate pygame and give permission 
 # to use pygame's functionality. 
 pygame.init() 
-  
+
 # define the RGB value 
 # for white colour 
 black = (0, 0, 0)
@@ -18,19 +20,38 @@ X = 698
 Y = 340
   
 # create the display surface object 
-# of specific dimension..e(X, Y). 
+# of specific dimension..e(X, Y).
+pygame.display.set_mode((X, Y ))
 display_surface = pygame.display.set_mode((X, Y )) 
-  
+clock= pygame.time.Clock()
+
 # set the pygame window name 
 pygame.display.set_caption('Title Screen') 
   
 # create a surface object, image is drawn on it. 
 image = pygame.image.load(r'C:\Users\dmccann\Documents\Final Project Babyy\final-project-fissssh-dylan-m\src\img\Test_Opening.png') 
 image2 = pygame.image.load(r'C:\Users\dmccann\Documents\Final Project Babyy\final-project-fissssh-dylan-m\src\img\FishingMan.jpg')
+fish1 = pygame.image.load(r'C:\Users\dmccann\Documents\Final Project Babyy\final-project-fissssh-dylan-m\src\img\Fish1.png')
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+    gameDisplay.blit(fish1,(x,y))
+
+
 done = False
 intro_page = 1
 display_intro = True
 
+class fish(object):
+    
+
+    def __init__(self,x,y,width,height,end):
+        self.x = random.randint(0, display_width)
+        self.y = random.randint(0, display_height)
+        self.width = width
+        self.height = height
+        self.end= end
+                
 while not done and display_intro:
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
@@ -49,23 +70,31 @@ while not done and display_intro:
          display_surface.blit(image, (0, 0)) 
          pygame.display.update()
 
+enemies = []
+spawn_counter = 30
+ 
 
 # infinite loop 
 while True : 
     # iterate over the list of Event objects 
-    # that was returned by pygame.event.get() method. 
+    # that was returned by pygame.event.get() method.   
     display_surface = pygame.display.set_mode((1000, 670 ))
-    display_surface.fill(black)
-    display_surface.blit(image2, (0, 0)) 
-    pygame.display.update()
-    
+    display_surface.blit(image2, (0, 0))
     print(event)
     for event in pygame.event.get() : 
+        # Spawn enemies if counter <= 0 then reset it.
+        spawn_counter -= 1
+        if spawn_counter <= 0:
+            # Append an enemy rect. You can pass the position directly as an argument.
+            enemies.append(fish1.get_rect(topleft=(random.randrange(600), 0)))
+            spawn_counter = 30
+        
+        for enemy_rect in enemies:
+            display_surface.blit(fish1, enemy_rect)
+        pygame.display.flip()
+        pygame.display.update()
         
 
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-        
         # if event object type is QUIT 
         # then quitting the pygame 
         # and program both. 
@@ -77,5 +106,4 @@ while True :
             # quit the program. 
             quit() 
   
-        # Draws the surface object to the screen.   
-       
+        # Draws the surface object to the screen. 
